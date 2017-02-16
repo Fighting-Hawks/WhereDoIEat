899633e604d861efae7906884fd048f029cf6670
Kevin <kismanta@sfu.ca>
2017-02-15 18:53:59 -0800

    Page1 -> HomePage

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -10,7 +10,7 @@ import {Geolocation} from 'ionic-native';
   selector: 'page-page1',
   templateUrl: 'page1.html'
 })
-export class Page1 {
+export class HomePage {
 
   constructor(public navCtrl: NavController, public loadingCtrl:LoadingController) {
 
