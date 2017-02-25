1b61aef6ddd3271e76d46afd390150abf8d0a470
Kevin <kismanta@sfu.ca>
2017-02-25 15:30:27 -0800

    Refactor code (remove loading spinner)

--- src/pages/HomePage/HomePage.ts
+++ src/pages/HomePage/HomePage.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
-import { LoadingController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
 import { MapPagePage } from '../map-page/map-page';
 
@@ -16,22 +15,12 @@ export class HomePage {
   longitude : 0;
   latitude : 0;
 
-  constructor(public navCtrl: NavController, public loadingCtrl:LoadingController) {
-
-    //var isSuccessful = false;
-    let loading = this.loadingCtrl.create({
-      content:'Please wait...'
-    });
-
-    loading.present();
+  constructor(public navCtrl: NavController) {
     let currentLocation = Geolocation.getCurrentPosition(()=>{
     }).then((location) => {
-      //isSuccessful = true;
-      loading.dismiss();
       this.setLongitude(location.coords.longitude);
       this.setLatitude(location.coords.latitude);
       console.log("latitude: " + this.latitude +" longitude: "+ this.longitude);
-      //this.pushCoordinates();
     }).catch((error) => {
       error = 'Error getting location';
       throw error;
@@ -49,21 +38,10 @@ export class HomePage {
      this.longitude = longitude;
   }
 
-  /*Returns device's current location
-  getLocation(){
-
-
-  }
-  */
-
-
-
-    //Retrieve a list of restaurants based on the device location using YELP API
-
+  //Retrieve a list of restaurants based on the device location using YELP API
   getRestaurants(long,lat){
 
   }
-
 
   pushCoordinates(){
     this.navCtrl.setRoot(MapPagePage,{
